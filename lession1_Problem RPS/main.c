95ac0178bc7a2f6fdad7e03aa9ded042651e1799
Antonim <111636301+kefearka@users.noreply.github.com>
2024-08-29 11:56:11 +0500

    Update main.c

--- lession1_Problem RPS/main.c
+++ lession1_Problem RPS/main.c
@@ -6,7 +6,8 @@
         
     Например, 2⬆️⬆️4 = 2 ^ (2 ^ (2 ^ 2)) = 2 ^ 16 = 65536.
     (2⬆️⬆️4) mod 10 = 6
-       
+    (3⬆️⬆️3) mod 6 = 3
+    (6⬆️⬆️2) mod 5 = 1
     Задача посчитать (a⬆️⬆️b) mod m.
 
     Решил сам
@@ -16,7 +17,11 @@
 
 unsigned spow_mod(unsigned n, unsigned k, unsigned m)
 {
-	
+    unsigned result = 1;
+	for(;k > 0; --k)
+	    result *= n % m;
+	    
+	return result % m;
 }
 
 int main()
@@ -25,7 +30,7 @@ int main()
 
     printf("Введите целые положительные - число, степень и основание модуля, через пробел\n");
 
-    if(scanf("%u%u%u", &a, &b, &c) == 3)
+    if(scanf("%u%u%u", &a, &b, &c) == 3 && a >= 0 && b >= 0)
     	printf("Результат вычисления = %u\n", spow_mod(a, b, c));
     else
     {
